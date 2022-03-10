d4e9e8e5c509ea503cb49f909b123bb9dc3f25cb
amirrmonfared <90863696+amirrmonfared@users.noreply.github.com>
2022-03-10 19:16:19 +0300

    Create transfer api

--- api/server.go
+++ api/server.go
@@ -3,11 +3,13 @@ package api
 import (
 	db "github.com/amirrmonfared/bank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
-// Server serves HTTp request for our banking service 
+// Server serves HTTP requests for our banking service.
 type Server struct {
-	store db.Store
+	store  db.Store
 	router *gin.Engine
 }
 
@@ -16,15 +18,21 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts", server.listAccount)
-	
+	router.GET("/accounts", server.listAccounts)
+
+	router.POST("/transfers", server.createTransfer)
+
 	server.router = router
 	return server
 }
 
-//Start runs the HTTP server on a specific address
+// Start runs the HTTP server on a specific address.
 func (server *Server) Start(address string) error {
 	return server.router.Run(address)
 }
@@ -32,4 +40,3 @@ func (server *Server) Start(address string) error {
 func errorResponse(err error) gin.H {
 	return gin.H{"error": err.Error()}
 }
-
